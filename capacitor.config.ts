bc5fb0896326f88c89ffabef674e2a91631ae36e
moavizq5555 <50484564+SyedMuhammadAli008@users.noreply.github.com>
2022-11-26 13:57:45 +0500

    donee

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'crowd',
-  webDir: 'www',
-  bundledWebRuntime: false
+  appId: "io.crowd.mi",
+  appName: "MiCrowd",
+  webDir: "www",
+  bundledWebRuntime: false,
 };
 
 export default config;
