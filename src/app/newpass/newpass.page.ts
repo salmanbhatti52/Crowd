b65bcdb71cf26842a9b50e226356dc76a431ed59
moavizq5555 <50484564+SyedMuhammadAli008@users.noreply.github.com>
2022-12-12 13:04:51 +0500

    changes done
    changes done

--- src/app/newpass/newpass.page.ts
+++ src/app/newpass/newpass.page.ts
@@ -7,11 +7,21 @@ import { Router } from "@angular/router";
   styleUrls: ["./newpass.page.scss"],
 })
 export class NewpassPage implements OnInit {
+  showPass1 = false;
+  showPass2 = false;
+
   constructor(public router: Router) {}
 
   ngOnInit() {}
 
   goToLogin() {
     this.router.navigate(["login"]);
+  }
+
+  togglePass1() {
+    this.showPass1 = !this.showPass1;
+  }
+  togglePass2() {
+    this.showPass2 = !this.showPass2;
   }
 }
