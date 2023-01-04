cc25e9e71bfff75190984126ca5f4c918dc54146
moavizq5555 <50484564+SyedMuhammadAli008@users.noreply.github.com>
2023-01-04 16:00:09 +0500

    mapmap test

--- src/app/locationmap/locationmap.module.ts
+++ src/app/locationmap/locationmap.module.ts
@@ -9,6 +9,7 @@ import { LocationmapPageRoutingModule } from "./locationmap-routing.module";
 import { LocationmapPage } from "./locationmap.page";
 
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { GoogleMapsModule } from "@angular/google-maps";
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
     FormsModule,
     IonicModule,
     LocationmapPageRoutingModule,
+    GoogleMapsModule,
   ],
   declarations: [LocationmapPage],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- src/app/locationmap/locationmap.page.ts
+++ src/app/locationmap/locationmap.page.ts
@@ -1,10 +1,22 @@
 import { PininfoPage } from "./../pininfo/pininfo.page";
 import { RestService } from "./../rest.service";
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { Router } from "@angular/router";
 
-import { GoogleMap } from "@capacitor/google-maps";
 import { ModalController } from "@ionic/angular";
+import {
+  GoogleMap,
+  MapInfoWindow,
+  MapGeocoder,
+  MapGeocoderResponse,
+  MapMarker,
+} from "@angular/google-maps";
 
 @Component({
   selector: "app-locationmap",
@@ -12,10 +24,36 @@ import { ModalController } from "@ionic/angular";
   styleUrls: ["./locationmap.page.scss"],
 })
 export class LocationmapPage implements OnInit {
+  ////angular map
+
+  @ViewChild("search")
+  public searchElementRef!: ElementRef;
+  @ViewChild("myGoogleMap", { static: false })
+  map!: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false })
+  info!: MapInfoWindow;
+
+  address = "";
+  latitude!: any;
+  longitude!: any;
+  zoom = 13;
+  maxZoom = 15;
+  minZoom = 8;
+  center!: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: true,
+    disableDoubleClickZoom: true,
+    mapTypeId: "roadmap",
+    disableDefaultUI: true,
+  };
+  markers = [] as any;
+
+  //// angular map
   showfilter = false;
 
   @ViewChild("map") mapRef: any = ElementRef;
-  map: any = GoogleMap;
+  // map: any = GoogleMap;
   title = "Title here";
   venuarr: any = "";
   venuarrOrg: any = "";
@@ -57,10 +95,10 @@ export class LocationmapPage implements OnInit {
   constructor(
     public router: Router,
     public rest: RestService,
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    private ngZone: NgZone,
+    private geoCoder: MapGeocoder
   ) {}
-
-  ngOnInit() {}
 
   ionViewDidEnter() {
     this.createMap();
@@ -73,40 +111,40 @@ export class LocationmapPage implements OnInit {
 
   ionViewWillLeave() {
     // enable the root left menu when leaving this page
-    this.map.destroy();
+    // this.map.destroy();
   }
 
   async createMap() {
     this.dismissmodal = 0;
     // AIzaSyAncWVozZi9mUrnaxdDJJE_rgRY5M-wD54
-    this.map = await GoogleMap.create({
-      id: "my-map", // Unique identifier for this map instance
-      element: this.mapRef?.nativeElement, // reference to the capacitor-google-map element,
-      apiKey: "AIzaSyA7ks8X2YnLcxTuEC3qydL2adzA0NYbl6c", // Your Google Maps API Key
-      forceCreate: true,
-      config: {
-        center: {
-          // The initial position to be rendered by the map
-          lat: 30.2398469,
-          lng: 71.4703882,
-        },
+    // this.map = await GoogleMap.create({
+    //   id: "my-map", // Unique identifier for this map instance
+    //   element: this.mapRef?.nativeElement, // reference to the capacitor-google-map element,
+    //   apiKey: "AIzaSyA7ks8X2YnLcxTuEC3qydL2adzA0NYbl6c", // Your Google Maps API Key
+    //   forceCreate: true,
+    //   config: {
+    //     center: {
+    //       // The initial position to be rendered by the map
+    //       lat: 30.2398469,
+    //       lng: 71.4703882,
+    //     },
 
-        zoom: 12, // The initial zoom level to be rendered by the map
-      },
-    });
+    //     zoom: 12, // The initial zoom level to be rendered by the map
+    //   },
+    // });
 
-    this.addmarkers();
+    // this.addmarkers();
   }
 
-  async addmarkers() {
-    await this.map.addMarkers(this.markerscheck);
-    this.map.setOnMarkerClickListener(async (marker: any) => {
-      this.dismissmodal++;
-      this.title = marker.title;
-      this.filterArrypin(marker.title);
-    });
-    this.map.setOnInfoWindowClickListener(async (marker: any) => {});
-  }
+  // async addmarkers() {
+  //   await this.map.addMarkers(this.markerscheck);
+  //   this.map.setOnMarkerClickListener(async (marker: any) => {
+  //     this.dismissmodal++;
+  //     this.title = marker.title;
+  //     this.filterArrypin(marker.title);
+  //   });
+  //   this.map.setOnInfoWindowClickListener(async (marker: any) => {});
+  // }
 
   tab1Click() {
     this.HideFilter();
@@ -142,20 +180,24 @@ export class LocationmapPage implements OnInit {
     var newVenuArr = [];
     for (var i = 0; i < this.venuarr.length; i++) {
       var obj = {
-        coordinate: {
+        position: {
           lat: this.venuarr[i].lattitude,
           lng: this.venuarr[i].longitude,
         },
-        snippet: this.venuarr[i].public_check_ins,
-        title: this.venuarr[i].name,
+        title: "" + this.venuarr[i].public_check_ins,
+        name: this.venuarr[i].name,
+        options: {
+          animation: google.maps.Animation.DROP,
+          draggable: false,
+        },
       };
 
       newVenuArr.push(obj);
     }
     this.venuarr = [];
     this.venuarr = newVenuArr;
-    this.markerscheck = this.venuarr;
-    this.map.destroy();
+    this.markers = this.venuarr;
+    // this.map.destroy();
 
     this.createMap();
   }
@@ -173,6 +215,7 @@ export class LocationmapPage implements OnInit {
   }
 
   async showHideFilterN() {
+    this.searchObject = "";
     if (this.showfilter) {
       this.showfilter = false;
     } else {
@@ -199,20 +242,24 @@ export class LocationmapPage implements OnInit {
     var newVenuArr = [];
     for (var i = 0; i < this.venuarr.length; i++) {
       var obj = {
-        coordinate: {
+        position: {
           lat: this.venuarr[i].lattitude,
           lng: this.venuarr[i].longitude,
         },
-        snippet: this.venuarr[i].public_check_ins,
-        title: this.venuarr[i].name,
+        title: "" + this.venuarr[i].public_check_ins,
+        name: this.venuarr[i].name,
+        options: {
+          animation: google.maps.Animation.DROP,
+          draggable: false,
+        },
       };
-
       newVenuArr.push(obj);
     }
+
     this.venuarr = [];
     this.venuarr = newVenuArr;
-    this.markerscheck = this.venuarr;
-    this.map.destroy();
+    this.markers = this.venuarr;
+    // this.map.destroy();
 
     this.createMap();
   }
@@ -221,21 +268,26 @@ export class LocationmapPage implements OnInit {
     this.venuarr = [];
     for (var i = 0; i < this.venuarrOrg.length; i++) {
       var obj = {
-        coordinate: {
+        position: {
           lat: this.venuarrOrg[i].lattitude,
           lng: this.venuarrOrg[i].longitude,
         },
-        snippet: this.venuarrOrg[i].public_check_ins,
-        title: this.venuarrOrg[i].name,
+        title: "" + this.venuarrOrg[i].public_check_ins,
+        name: this.venuarrOrg[i].name,
+        options: {
+          animation: google.maps.Animation.DROP,
+          draggable: false,
+        },
       };
 
       this.venuarr.push(obj);
     }
 
-    this.markerscheck = this.venuarr;
+    this.markers = this.venuarr;
   }
 
   async HideFilter() {
+    this.searchObject = "";
     this.showfilter = false;
     console.log("modalopen-----", this.modalopen);
 
@@ -243,18 +295,6 @@ export class LocationmapPage implements OnInit {
       await this.modalCtrl.dismiss();
       this.modalopen = 0;
     }
-  }
-
-  async filterArrypin(searchTerm: any) {
-    for (var i = 0; i < this.venuarrOrg.length; i++) {
-      if (this.venuarrOrg[i].name.toLowerCase() == searchTerm.toLowerCase()) {
-        this.searchObject = this.venuarrOrg[i];
-      }
-    }
-
-    this.rest.pinobject = this.searchObject;
-
-    this.goTOinfopage();
   }
 
   async goTOinfopage() {
@@ -270,8 +310,152 @@ export class LocationmapPage implements OnInit {
   }
 
   gotodetail() {
-    this.HideFilter();
     this.rest.detail = this.searchObject;
+    this.HideFilter();
     this.router.navigate(["venuedetail"]);
+  }
+
+  ngAfterViewInit(): void {
+    // Binding autocomplete to search input control
+    // let autocomplete = new google.maps.places.Autocomplete(
+    //   this.searchElementRef.nativeElement
+    // );
+    // Align search box to center
+    // this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+    //   this.searchElementRef.nativeElement
+    // );
+    // autocomplete.addListener("place_changed", () => {
+    //   this.ngZone.run(() => {
+    //     //get the place result
+    //     let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+    //     //verify result
+    //     if (place.geometry === undefined || place.geometry === null) {
+    //       return;
+    //     }
+
+    //     console.log({ place }, place.geometry.location?.lat());
+
+    //     //set latitude, longitude and zoom
+    //     this.latitude = place.geometry.location?.lat();
+    //     this.longitude = place.geometry.location?.lng();
+
+    //     // Set marker position
+    //     this.setMarkerPosition(30.048418, 72.3129359);
+
+    //     this.center = {
+    //       lat: 30.048418,
+    //       lng: 72.3129359,
+    //     };
+    //   });
+    // });
+
+    this.setMarkerPosition(30.2088837, 71.4689853);
+
+    this.center = {
+      lat: 30.2088837,
+      lng: 71.4689853,
+    };
+  }
+
+  ngOnInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.center = {
+        lat: 30.2088837,
+        lng: 71.4689853,
+      };
+      // Set marker position
+      this.setMarkerPosition(30.2088837, 71.4689853);
+    });
+  }
+
+  setMarkerPosition(latitude: any, longitude: any) {
+    // Set marker position
+
+    this.venuarr = [];
+    for (var i = 0; i < this.venuarrOrg.length; i++) {
+      var obj = {
+        position: {
+          lat: this.venuarr[i].lattitude,
+          lng: this.venuarr[i].longitude,
+        },
+        title: "" + this.venuarr[i].public_check_ins,
+        name: this.venuarr[i].name,
+        options: {
+          animation: google.maps.Animation.DROP,
+          draggable: false,
+        },
+      };
+
+      this.venuarr.push(obj);
+    }
+
+    this.markers = this.venuarr;
+  }
+
+  eventHandler(event: any, name: string) {
+    // console.log(event, name);
+
+    switch (name) {
+      case "mapDblclick": // Add marker on double click event
+        break;
+
+      case "mapDragMarker":
+        break;
+
+      case "mapDragend":
+        this.getAddress(event.latLng.lat(), event.latLng.lng());
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  getAddress(latitude: any, longitude: any) {
+    this.geoCoder
+      .geocode({ location: { lat: latitude, lng: longitude } })
+      .subscribe((addr: MapGeocoderResponse) => {
+        if (addr.status === "OK") {
+          if (addr.results[0]) {
+            this.zoom = 13;
+            this.address = addr.results[0].formatted_address;
+          } else {
+            this.address = "";
+            window.alert("No results found");
+          }
+        } else {
+          this.address = "";
+          window.alert("Geocoder failed due to: " + addr.status);
+        }
+      });
+  }
+
+  @ViewChild(MapInfoWindow, { static: false })
+  infoWindow!: MapInfoWindow;
+
+  infoContent: string | undefined;
+
+  openInfo(marker: MapMarker, content: string, markerobj: any) {
+    this.infoContent = content;
+    this.infoWindow.open(marker);
+    console.log("markerobj-----------", markerobj);
+    console.log("content-----------", content);
+
+    this.filterArrypin(markerobj.name);
+  }
+
+  async filterArrypin(searchTerm: any) {
+    for (var i = 0; i < this.venuarrOrg.length; i++) {
+      if (this.venuarrOrg[i].name.toLowerCase() == searchTerm.toLowerCase()) {
+        this.searchObject = this.venuarrOrg[i];
+      }
+    }
+
+    this.rest.pinobject = this.searchObject;
+
+    // this.goTOinfopage();
   }
 }
