60fcc27e8b6ff33ec057fb1a9580f89d7ba7d715
moavizq5555 <50484564+SyedMuhammadAli008@users.noreply.github.com>
2023-02-14 12:18:27 +0500

    venu option done

--- src/app/select-venue-popup/select-venue-popup.page.ts
+++ src/app/select-venue-popup/select-venue-popup.page.ts
@@ -1,33 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-select-venue-popup',
-  templateUrl: './select-venue-popup.page.html',
-  styleUrls: ['./select-venue-popup.page.scss'],
+  selector: "app-select-venue-popup",
+  templateUrl: "./select-venue-popup.page.html",
+  styleUrls: ["./select-venue-popup.page.scss"],
 })
 export class SelectVenuePopupPage implements OnInit {
-  venue_list:any;
+  venue_list: any;
   value: any;
-  constructor(public modalCtrlr:ModalController) { }
+  constructor(public modalCtrlr: ModalController) {}
 
   ngOnInit() {
-    console.log("Receieved list:",this.venue_list);
-    
+    console.log("Receieved list:", this.venue_list);
   }
 
-  selectedValue(ev:any){
-    console.log(ev);
-    this.value = ev.detail.value
-
+  selectedValue(ev: any) {
+    console.log("aliiiiii", ev.detail.value);
+    this.value = ev.detail.value;
   }
 
-  sendValue(){
-    return this.modalCtrlr.dismiss(this.value, 'value_sent');
+  sendValue(val: any) {
+    return this.modalCtrlr.dismiss(val, "value_sent");
   }
 
-  dismiss(){
+  dismiss() {
     this.modalCtrlr.dismiss();
   }
-  
 }
