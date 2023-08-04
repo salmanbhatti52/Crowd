75a75347fa9f56c1adae277448a2a8d50bf9fa6f
Ali Gharsheen <ali.gharsheen1072@gmail.com>
2023-08-04 07:52:45 +0500

    code add

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -3,9 +3,9 @@ import { Location } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { ModalController, NavController } from "@ionic/angular";
-// import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
 import { RestService } from "../rest.service";
-import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+// import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -42,7 +42,7 @@ export class ProfilePage implements OnInit {
     public navCtrl: NavController,
     public rest: RestService,
     public modalCtrl: ModalController,
-    private camera: Camera,
+    // private camera: Camera,
     private alertController: AlertController
   ) {}
 
@@ -89,17 +89,10 @@ export class ProfilePage implements OnInit {
     })
   }
   ngOnInit() {
-    // setTimeout(async () => {
-    //   const result = await Camera.checkPermissions();
-    //   console.log("check permsisson result: ",result);
-    //   // if(result.photos !== 'granted'){
-    //   //   // result.photos = 'granted';
-    //   //   const result1 = (await Camera.requestPermissions()).photos
-    //   //   console.log("Result 1 Photos: ",result1);
-        
-    //   // }
-    //   // console.log("Updated permsisson result: ",result);
-    // }, 500);
+    setTimeout(async () => {
+      const result = await Camera.checkPermissions();
+      console.log("check permsisson result: ",result);
+    }, 500);
     // setTimeout(() => {
     //   Camera.requestPermissions()
     //   const result = Camera.checkPermissions();
@@ -234,92 +227,92 @@ export class ProfilePage implements OnInit {
     console.log("save");
   }
 
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'Choose From',
-      buttons: [
-        {
-          text: 'Camera',
-          role: 'camera',
-          handler: () => {
-            this.handlerMessage = 'Use Camera';
-            this.addimg('camera');
-          },
-        },
-        {
-          text: 'Gallery',
-          role: 'gallery',
-          handler: () => {
-            this.handlerMessage = 'Use Gallery';
-            this.addimg('gallery');
+  // async presentAlert() {
+  //   const alert = await this.alertController.create({
+  //     header: 'Choose From',
+  //     buttons: [
+  //       {
+  //         text: 'Camera',
+  //         role: 'camera',
+  //         handler: () => {
+  //           this.handlerMessage = 'Use Camera';
+  //           this.addimg('camera');
+  //         },
+  //       },
+  //       {
+  //         text: 'Gallery',
+  //         role: 'gallery',
+  //         handler: () => {
+  //           this.handlerMessage = 'Use Gallery';
+  //           this.addimg('gallery');
 
-          },
-        },
-      ],
-    });
+  //         },
+  //       },
+  //     ],
+  //   });
 
-    await alert.present();
+  //   await alert.present();
 
-    const { role } = await alert.onDidDismiss();
-    this.roleMessage = `Dismissed with role: ${role}`;
-  }
+  //   const { role } = await alert.onDidDismiss();
+  //   this.roleMessage = `Dismissed with role: ${role}`;
+  // }
 
-  async addimg(data:any) {
+  async addimg() {
     console.log("addd imge");
 
-    if(data == 'camera'){
-      console.log(data);
+    // if(data == 'camera'){
+    //   console.log(data);
       
-      const options: CameraOptions = {
-        quality: 100,
-        destinationType: this.camera.DestinationType.DATA_URL,
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE,
-        sourceType:this.camera.PictureSourceType.CAMERA
-      }
-      this.camera.getPicture(options).then((imageData) => {
-        // imageData is either a base64 encoded string or a file URI
-        // If it's base64 (DATA_URL):
-        let base64Image = 'data:image/jpeg;base64,' + imageData;
-        console.log("imageData",imageData);
-        console.log("base64Image",base64Image);
+    //   const options: CameraOptions = {
+    //     quality: 100,
+    //     destinationType: this.camera.DestinationType.DATA_URL,
+    //     encodingType: this.camera.EncodingType.JPEG,
+    //     mediaType: this.camera.MediaType.PICTURE,
+    //     sourceType:this.camera.PictureSourceType.CAMERA
+    //   }
+    //   this.camera.getPicture(options).then((imageData) => {
+    //     // imageData is either a base64 encoded string or a file URI
+    //     // If it's base64 (DATA_URL):
+    //     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    //     console.log("imageData",imageData);
+    //     console.log("base64Image",base64Image);
         
-        // this.imgdata = image.base64String;
-      // this.imgdataComing = "data:image/jpeg;base64," + image.base64String;
+    //     // this.imgdata = image.base64String;
+    //   // this.imgdataComing = "data:image/jpeg;base64," + image.base64String;
   
-      // console.log("incoming img----", this.imgdata);
-      // this.imageupdate = 1;
-       }, (err) => {
-        console.log("err camera", err);
+    //   // console.log("incoming img----", this.imgdata);
+    //   // this.imageupdate = 1;
+    //    }, (err) => {
+    //     console.log("err camera", err);
         
-        // Handle error
-       });    
-    }
+    //     // Handle error
+    //    });    
+    // }
     
-    else if(data == 'gallery'){
-      console.log(data);
+    // else if(data == 'gallery'){
+    //   console.log(data);
       
-      const options: CameraOptions = {
-        quality: 100,
-        destinationType: this.camera.DestinationType.DATA_URL,
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE,
-        sourceType:this.camera.PictureSourceType.PHOTOLIBRARY
-      }
-      this.camera.getPicture(options).then((imageData) => {
-        // imageData is either a base64 encoded string or a file URI
-        // If it's base64 (DATA_URL):
-        let base64Image = 'data:image/jpeg;base64,' + imageData;
-        console.log("imageData",imageData);
-        console.log("base64Image",base64Image);
-       }, (err) => {
-        console.log("err gallery", err);
-       });    
-    }
+    //   const options: CameraOptions = {
+    //     quality: 100,
+    //     destinationType: this.camera.DestinationType.DATA_URL,
+    //     encodingType: this.camera.EncodingType.JPEG,
+    //     mediaType: this.camera.MediaType.PICTURE,
+    //     sourceType:this.camera.PictureSourceType.PHOTOLIBRARY
+    //   }
+    //   this.camera.getPicture(options).then((imageData) => {
+    //     // imageData is either a base64 encoded string or a file URI
+    //     // If it's base64 (DATA_URL):
+    //     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    //     console.log("imageData",imageData);
+    //     console.log("base64Image",base64Image);
+    //    }, (err) => {
+    //     console.log("err gallery", err);
+    //    });    
+    // }
     
-    else{
+    // else{
 
-    }
+    // }
     
 
     
@@ -332,21 +325,21 @@ export class ProfilePage implements OnInit {
       
 
     // }else{
-      // const image = await Camera.getPhoto({
-      //   quality: 90,
-      //   allowEditing: true,
-      //   resultType: CameraResultType.Base64,
-      //   source: CameraSource.Prompt,
-      // });
-      // console.log(image);
-      // this.imgdata = image.base64String;
-      // this.imgdataComing = "data:image/jpeg;base64," + image.base64String;
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Base64,
+        source: CameraSource.Prompt,
+      });
+      console.log(image);
+      this.imgdata = image.base64String;
+      this.imgdataComing = "data:image/jpeg;base64," + image.base64String;
   
-      // console.log("incoming img----", this.imgdata);
-      // this.imageupdate = 1;
+      console.log("incoming img----", this.imgdata);
+      this.imageupdate = 1;
+      // Can be set to the src of an image now
     // }
     
-    // Can be set to the src of an image now
   }
 
   gotoReservation(){
