6095b100b28938072f5766777fcdac2b3c613648
Ali Gharsheen <88122412+Ali-Gharsheen@users.noreply.github.com>
2025-01-29 18:59:58 +0500

    code add

--- src/app/privacy-policy/privacy-policy.page.ts
+++ src/app/privacy-policy/privacy-policy.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { RestService } from '../rest.service';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -7,14 +9,34 @@ import { Location } from '@angular/common';
   styleUrls: ['./privacy-policy.page.scss'],
 })
 export class PrivacyPolicyPage implements OnInit {
-
-  constructor(public location:Location) { }
+  privacyPolicy:any;
+  constructor(public location:Location,public rest:RestService) { }
 
   ngOnInit() {
+    this.getPrivacyPolicy();
   }
 
   goBack(){
     this.location.back();
   }
+
+  getPrivacyPolicy(){
+    this.rest.presentLoader();
+    this.rest.getRequest("get_privacy_policy").subscribe((res:any)=>{
+      this.rest.dismissLoader();
+      console.log("Privacy Policy : ",res);
+      this.privacyPolicy = res.data[0];
+      console.log("Privacy Policy : ",this.privacyPolicy);
+    })
+  }
+
+  getDateSlashFormat(val:any){
+      if(val){
+        return format(new Date(val), 'M/d/yyyy');
+      }
+      else{
+        return val;
+      }
+    }
 
 }
